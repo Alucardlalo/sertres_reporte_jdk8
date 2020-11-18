cdf41e094238521089ba492f166d4e1594087dd6
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-11-18 09:54:18 -0600

    first comitt and prueba de inicializacion

---
+++ src/main/java/com/sertres/reporte/prueba.java
@@ -0,0 +1,16 @@
+package com.sertres.reporte;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class prueba {
+
+    @GetMapping("/prueba")
+    public String prueba(){
+        return "funciona el spring";
+    }
+}
