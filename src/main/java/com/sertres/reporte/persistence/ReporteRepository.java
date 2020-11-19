6df378ce597b4c3b0dd9384d1b0cdf7ce43d4384
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-11-19 14:20:42 -0600

    add anotation @Repository in ReportRepository

--- src/main/java/com/sertres/reporte/persistence/ReporteRepository.java
+++ src/main/java/com/sertres/reporte/persistence/ReporteRepository.java
@@ -2,9 +2,11 @@ package com.sertres.reporte.persistence;
 
 import com.sertres.reporte.persistence.crud.ReporteCrudRepository;
 import com.sertres.reporte.persistence.entity.Reporte;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ReporteRepository {
     private ReporteCrudRepository reporteCrudRepository;
 
