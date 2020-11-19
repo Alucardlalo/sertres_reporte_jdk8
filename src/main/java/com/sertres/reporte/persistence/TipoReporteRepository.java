9500071e2efb6a055ceb64136a65630e99090ac6
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-11-19 09:50:44 -0600

    implementacion de primer repositorio tipoReporte

--- src/main/java/com/sertres/reporte/persistence/TipoReporteRepository.java
+++ src/main/java/com/sertres/reporte/persistence/TipoReporteRepository.java
@@ -2,9 +2,12 @@ package com.sertres.reporte.persistence;
 
 import com.sertres.reporte.persistence.crud.TipoReporteCrudRepository;
 import com.sertres.reporte.persistence.entity.TipoReporte;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class TipoReporteRepository {
     private TipoReporteCrudRepository tipoReporteCrudRepository;
 
@@ -13,8 +16,23 @@ public class TipoReporteRepository {
         return (List<TipoReporte>) tipoReporteCrudRepository.findAll();
     }
 
-    //metodo de querry method para traer tipo de reporte por id especifico
+    //querry method para traer tipo de reporte por id especifico
     public List<TipoReporte> GetByIdTipoReporte(int idTipoReporte){
         return tipoReporteCrudRepository.findByIdTipoReporte(idTipoReporte);
+    }
+
+    //retorna un tipo reporte especifico
+    public Optional<TipoReporte> GetTipoReporte(int idTipoReporte){
+        return tipoReporteCrudRepository.findById(idTipoReporte);
+    }
+
+    //salvar un nuevo tipo de reporte
+    public TipoReporte saveTipoReporte(TipoReporte tipoReporte){
+        return tipoReporteCrudRepository.save(tipoReporte);
+    }
+
+    //eliminar un tipo de reporte mediante clave primaria
+    public void eliminarTipoReporte(int idTipoReporte){
+        tipoReporteCrudRepository.deleteById(idTipoReporte);
     }
 }
