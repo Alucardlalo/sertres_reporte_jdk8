20b0880cd2916d866f30be28d20cad35fb4a50ee
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-11-20 17:07:30 -0600

    create reportService

---
+++ src/main/java/com/sertres/reporte/domain/service/ReportService.java
@@ -0,0 +1,47 @@
+package com.sertres.reporte.domain.service;
+
+import com.sertres.reporte.domain.Report;
+import com.sertres.reporte.domain.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    //todos los reportes
+    public List<Report> getAll(){
+        return reportRepository.getAll();
+    }
+
+    public List<Report> getByReport(int reportId){
+        return reportRepository.GetByReport(reportId);
+    }
+
+    public List<Report> getByReportName(String reportName){
+        return reportRepository.GetByNameReport(reportName);
+    }
+
+    //save and delete
+    public Report save(Report report){
+        return reportRepository.save(report);
+    }
+
+    public void delete(int reportId){
+        reportRepository.delete(reportId);
+    }
+
+    /*se debe cambiar por un opcional en reportRepository
+    //delete real
+    public boolean delete(int reportID) {
+        return getByReport(reportId).stream().map(Report -> {
+            reportRepository.delete(reportId);
+            return true;
+        }).orElse(False);
+    }*/
+}
